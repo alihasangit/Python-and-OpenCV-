cc5e775de0d6a31a1632efe18b18afa80fd2d2cc
Ali <70559023+alihasangit@users.noreply.github.com>
2020-09-28 21:30:24 +0500

    Add files via upload

---
+++ detectimage.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+NumPlate = cv2.CascadeClassifier('haarcascades\haarcascade_russian_plate_number.xml')
+
+img = cv2.imread('car.jpg')
+Vehicle= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+Num = NumPlate.detectMultiScale(Vehicle,scaleFactor=1.2,
+    minNeighbors = 5, minSize=(5,5))
+
+
+cv2.imshow('Car',Vehicle)
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
