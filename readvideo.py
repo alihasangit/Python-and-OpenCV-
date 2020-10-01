1a9ca2de79a726263df505c40638e66955cda912
Ahmed <70559023+alihasangit@users.noreply.github.com>
2020-10-01 13:57:15 +0500

    Add files via upload

---
+++ readvideo.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+
+
+PlateNum= cv2.CascadeClassifier('haarcascades\haarcascade_russian_plate_number.xml')
+
+cap = cv2.VideoCapture('videoplayback.mp4')
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 80)
+
+
+if (cap.isOpened()==False):
+    print('Error Reading video')
+
+
+        
+    if ret == True:
+        cv2.imshow('Assignment',frame)
+    
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
